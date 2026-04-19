c4974eaf4ef9b8349e97c05a3b08feadd43efb9e
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-04-18 18:05:19 -0600

    🛡️ Extract LocalAuthGuard from inline AuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   login(@Request() req) {
     return req.user;
   }
