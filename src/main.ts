24480e9b5dba58f908156171876be7860f4a7222
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-05-04 13:38:00 -0600

    🔧 Register PrismaExceptionFilter as a global exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { PrismaExceptionFilter } from './prisma/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
