790f926d5e066b96af600050bc93f22d56fb6f5d
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-05-06 16:42:49 -0600

    :sparkles: Add global api prefix to all routes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, RequestMethod } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './prisma/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: ':code', method: RequestMethod.GET }],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
