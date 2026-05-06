05e5f6a01a68f1c08b61a4e5ccc553e46fb01ce2
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-05-06 16:42:57 -0600

    :recycle: Separate redirect into dedicated controller at /:code

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -5,16 +5,12 @@ import {
   Post,
   Request,
   UseGuards,
-  Param,
-  HttpStatus,
-  Res,
 } from '@nestjs/common';
 import { CreateShortLinkDto } from './dto/create-short-link.dto';
 import { ShortLink } from './interfaces/url.interfaces';
 import { UrlService } from './url.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { AuthenticatedUser } from 'src/auth/interfaces/auth.interfaces';
-import type { Response } from 'express';
 
 @Controller('url')
 export class UrlController {
@@ -32,21 +28,6 @@ export class UrlController {
       body.customAlias,
       body.expiresAt,
     );
-  }
-
-  @Get(':id')
-  async redirectUrl(
-    @Param('id') id: string,
-    @Res() response: Response,
-  ): Promise<void> {
-    const link = await this.urlService.findLongUrl(id);
-    if (link) {
-      return response.status(HttpStatus.FOUND).redirect(link.originalUrl);
-    } else {
-      response.status(HttpStatus.NOT_FOUND).json({
-        message: 'Link not valid',
-      });
-    }
   }
 
   @Get()
